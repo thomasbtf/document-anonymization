ae523cb676ebd154133c6b1a4af27e547d760bbc
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2021-04-28 09:55:44 +0200

    fmt

--- workflow/scripts/redact-page.py
+++ workflow/scripts/redact-page.py
@@ -26,10 +26,11 @@ def process_page(image_path: str, out_path: str, data_to_redact: str):
 
     cv2.imwrite(out_path, img)
 
+
 def add_watermark(img: typing.Any) -> typing.Any:
     x, y = 50, 50
     watermark_text = "anonymized by DocNo v0.1"
-    cv2.putText(img, watermark_text, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+    cv2.putText(img, watermark_text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
     return img
 
 

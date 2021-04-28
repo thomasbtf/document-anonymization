7db5d1db99c11f5b8d26b7a87626236ee4575705
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2021-04-28 09:55:22 +0200

    change watermark text

--- workflow/scripts/redact-page.py
+++ workflow/scripts/redact-page.py
@@ -28,7 +28,8 @@ def process_page(image_path: str, out_path: str, data_to_redact: str):
 
 def add_watermark(img: typing.Any) -> typing.Any:
     x, y = 50, 50
-    cv2.putText(img, "v0.1", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+    watermark_text = "anonymized by DocNo v0.1"
+    cv2.putText(img, watermark_text, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
     return img
 
 

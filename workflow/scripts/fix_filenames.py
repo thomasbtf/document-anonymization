b1b7dea45522a78b13e01b5f3b10a7368e6c9339
Thomas <thomas.battenfeld@gmail.com>
2021-06-01 07:41:02 +0000

    exclude snakemake timestamps, fix none append, add pdf detection and coversion

--- workflow/scripts/fix_filenames.py
+++ workflow/scripts/fix_filenames.py
@@ -1,37 +1,61 @@
 import os
 import sys
-import imghdr
+import filetype
+import pdf2image
 
 
 def add_ext(paths_file: str, fixed_paths: str):
-    with open(paths_file, 'r') as path_list:
-        with open(fixed_paths, 'w') as new_paths:
+    with open(paths_file, "r") as path_list:
+        with open(fixed_paths, "w") as new_paths:
             cwd = os.getcwd()
-            accepted_ext = ["jpg", "jpeg", "tiff", "tif", "bmp"] # may add "pdf" etc.
+            accepted_ext = ["jpg", "jpeg", "tiff", "tif", "bmp"]
             ext_pairs = [{"jpg", "jpeg"}, {"tiff", "tif"}]
+
             for path in path_list:
                 path = path.strip()
                 filedir, filename = os.path.split(path)
                 ext = filename.split(".")[-1]
-                ftype = imghdr.what(path)
+                ftype = filetype.guess(path).extension
+
                 if ext != ftype:
-                    if ext not in accepted_ext:
+                    #  file won´t be written to file-list for further processing
+                    if ftype is None:
+                        print(f"file {path} is in an incompatible file format.")
+
+                    # convert pdf
+                    elif ftype == "pdf":
+                        print("pdf")
+                        pages = pdf2image.convert_from_path(path)
+                        for i, page in enumerate(pages):
+                            new_paths.write(path + f"_{i}.tif\n")
+                        os.chdir(os.path.join(cwd, filedir))
+                        for i, page in enumerate(pages):
+                            page.save(f"{filename}_{i}.tif", "TIFF")
+                        os.chdir(cwd)
+                        os.remove(path)
+
+                    # change the img file type
+                    elif ext not in accepted_ext and ftype in accepted_ext:
+                        print("change extension")
                         new_paths.write(path + f".{ftype}\n")
                         os.chdir(os.path.join(cwd, filedir))
                         os.rename(filename, filename + f".{ftype}")
                         os.chdir(cwd)
+
+                    # This elif clause allows to leave files with alternative but adequate extension untouched
                     elif set((ext, ftype)) in ext_pairs:
-                        # This elif clause allows to leave files with alternative but adequate extension untouched
+                        print("set((ext, ftype)) in ext_pairs")
                         new_paths.write(path + "\n")
+
+                    # This elif clause allows to specify what shall happen to the file in question.
+                    # This will become relevant if we are going to allow non-img file-types like pdf.
+                    # In this case the file needs to be channeled into another branch of the workflow.
                     elif ftype == "None" and ext in accepted_ext:
                         print(f"file {path} is not an image file.")
-                        # This elif clause allows to specify what shall happen to the file in question.
-                        # This will become relevant if we are going to allow non-img file-types like pdf.
-                        # In this case the file needs to be channeled into another branch of the workflow.
-                    else:
-                        print(f"file {path} is in an incompatible file format.")
-                        # file won´t be written to file-list for further processing
+
+                # file extension equals equals the detected extension
                 else:
+                    print("else")
                     new_paths.write(path + "\n")
 
 
--- workflow/scripts/scan_decomp_data.py
+++ workflow/scripts/scan_decomp_data.py
@@ -8,7 +8,9 @@ def scan_folder(subfolder_path: str, writeable_file_object: typing.TextIO):
     for entry in os.scandir(subfolder_path):
         if entry.is_dir(follow_symlinks=False):
             scan_folder(entry.path, writeable_file_object)
-        elif entry.is_file() and not any(ignore_element in entry.path for ignore_element in ignore):
+        elif entry.is_file() and not any(
+            ignore_element in entry.path for ignore_element in ignore
+        ):
             writeable_file_object.write(f"{entry.path}\n")
         else:
             pass
